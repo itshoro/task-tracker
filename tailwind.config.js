83bb79b2f34357d4d5826af283d06b6a3b78002a
Tim Nelke <tim.nelke@outlook.com>
2023-06-08 19:18:57 +0200

    Update tailwind.config.js - dark mode is no longer applied by media query, but by class - add missing animation for Dialog component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      animation: {
+        dialog: "dialog 300ms ease-out 1",
+      },
+      keyframes: {
+        dialog: {
+          "0%": { transform: "translateY(2rem) scale(0.75)", opacity: "0" },
+          "25%": { opacity: 0 },
+          "75%": { transform: "scale(1.05)" },
+          "100%": { transform: "translateY(0) scale(1)", opacity: "100%" },
+        },
       },
     },
   },
   plugins: [],
-}
+};
